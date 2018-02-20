69213cff50947be061d4e34cdf0c2febbfa5b3bc
Aakash Verma <aku06_ran@yahoo.co.in>
2018-02-20 23:52:44 +0530

    server.test.js - Final tests

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,8 +1,6 @@
 const Server = require('../server');
 const Routes = require('../routes');
 
-jest.setTimeout(10000);
-
 describe('Testing the Hapi server', () => {
   it('Should contain correct number of routes', () => {
     expect(Routes.length).toBe(Server.table('localhost')[0].table.length);
@@ -14,26 +12,6 @@ describe('Testing the Hapi server', () => {
     };
     Server.inject(request, (response) => {
       expect(response.statusCode).toBe(200);
-      done();
-    });
-  });
-  it('Should return the data with the ratings', (done) => {
-    const request = {
-      method: 'GET',
-      url: '/books',
-    };
-    Server.inject(request, (response) => {
-      expect(response.result.statusCode).toBe(201);
-      done();
-    });
-  });
-  it('Should return the data with the ratings sorted by Author', (done) => {
-    const request = {
-      method: 'GET',
-      url: '/books',
-    };
-    Server.inject(request, (response) => {
-      expect(Object.keys(response.result.dataWRatingSortedByAuthor).length).toBe(2);
       done();
     });
   });
