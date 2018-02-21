a942b4393a060070fb104af4db1ac619249ff8f1
Aakash Verma <aku06_ran@yahoo.co.in>
2018-02-21 14:03:06 +0530

    read functionality

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,6 @@ const books = require('./books');
 const storeRatings = require('./storeRatings');
 const like = require('./like');
 const dislike = require('./dislike');
+const read = require('./read');
 
-module.exports = [].concat(books, storeRatings, like, dislike);
+module.exports = [].concat(books, storeRatings, like, dislike, read);
